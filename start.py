491fc492bbb07d207e5c46a956f086e172100662
Aniket Karne <aniket.karne@hitachiconsulting.com>
2019-03-26 18:54:04 +0530

    added desktop automation for muting and unmiting skype when the call is connected

--- start.py
+++ start.py
@@ -2,8 +2,8 @@ import speech_recognition as sr
 from guessing_game import recognize_speech_from_mic
 import time
 import vlc
-
-
+from sikuli import *
+from lackey import *
 # player = vlc.MediaPlayer("Status.mp3")
 # player.play()
 # time.sleep(10)
@@ -23,6 +23,7 @@ while not a:
 
     if compareText is not None and compareText.lower() == "aniket":
         print("inside loop")
+        click("unmute.png")
         vlc_instance = vlc.Instance()
         player = vlc_instance.media_player_new()
         media = vlc_instance.media_new("Status.mp3")
@@ -32,7 +33,14 @@ while not a:
         duration = player.get_length() / 1000
         print(duration)
         time.sleep(duration)
+        click("mute.png")
         a = True
     else:
         print(text['transcription'])
+
+#click("skype.png")
+# wait("mute.png", 5)# Maybe the Start menu is slow
+
+
+
 
